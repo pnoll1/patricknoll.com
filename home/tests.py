0b844752188f4523ddc83edf1b3b8bec3ac35d04
pnoll1 <8444617+pnoll1@users.noreply.github.com>
2019-06-06 22:38:18 -0700

    added path tests

--- home/tests.py
+++ home/tests.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+class ViewTests(TestCase):
+    def test_paths(self):
+        r = requests.get('http://localhost:8000')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/#contact')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/boogaliboo')
+        self.assertEqual(r.status_code, 404)
+        r = requests.get('http://localhost:8000/resume')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/resume?q=software')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/resume?q=mechanical')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/design')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/stress_analysis')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/3d_modeling')
+        self.assertEqual(r.status_code, 200)
+        r = requests.get('http://localhost:8000/programming')
+        self.assertEqual(r.status_code, 200)
