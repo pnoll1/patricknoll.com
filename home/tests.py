492a050b922a64fda944a5d7e5107d465d78f4be
pnoll1 <8444617+pnoll1@users.noreply.github.com>
2019-08-16 14:01:17 -0700

    updated services and changed tests to use django test client

--- home/tests.py
+++ home/tests.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
-import requests
+from django.test import TestCase, Client
 
 class ViewTests(TestCase):
     def test_paths(self):
-        r = requests.get('http://localhost:8000')
+        c = Client()
+        r = c.get('http://localhost:8000')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/#contact')
+        r = c.get('http://localhost:8000/#contact')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/boogaliboo')
+        r = c.get('http://localhost:8000/services')
+        self.assertEqual(r.status_code, 200)
+        r = c.get('http://localhost:8000/boogaliboo')
         self.assertEqual(r.status_code, 404)
-        r = requests.get('http://localhost:8000/resume')
+        r = c.get('http://localhost:8000/resume')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/resume?q=software')
+        r = c.get('http://localhost:8000/resume?q=software')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/resume?q=mechanical')
+        r = c.get('http://localhost:8000/resume?q=mechanical')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/design')
+        r = c.get('http://localhost:8000/design')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/stress_analysis')
+        r = c.get('http://localhost:8000/stress_analysis')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/3d_modeling')
+        r = c.get('http://localhost:8000/3d_modeling')
         self.assertEqual(r.status_code, 200)
-        r = requests.get('http://localhost:8000/programming')
+        r = c.get('http://localhost:8000/programming')
         self.assertEqual(r.status_code, 200)
