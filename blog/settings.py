be1b223db63f1c69d66183e76434da36df965152
pat <8444617+pnoll1@users.noreply.github.com>
2020-03-23 07:51:18 -0700

    setup for automated push to production

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from blog.secret_stuff import key, db_info
+from blog.secret_stuff import key, db_info, allowed_hosts, debug
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
