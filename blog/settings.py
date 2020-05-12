b625dde529f41c98130f5d6479f69e5902832a1c
pat <8444617+pnoll1@users.noreply.github.com>
2020-05-11 23:58:21 -0700

    setup collectstatic

--- blog/settings.py
+++ blog/settings.py
@@ -122,5 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
-STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ['home/static', 'downloads/static']
+
