7799e31e190c5a0da343b8c78ee0dcd3aa7a152b
pat <8444617+pnoll1@users.noreply.github.com>
2020-06-03 22:50:52 -0700

    update to new mapbox tile api

--- home/static/js/coverage_map.js
+++ home/static/js/coverage_map.js
@@ -1,11 +1,13 @@
 document.getElementById('coverage_map').classList.add('coverage_map');
 var baselayers, ways, nodes, controls_layers, mymap, overlays, mapbox, featureCount;
 
-var mapbox = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    tileSize: 512,
     minZoom: 4,
     maxZoom: 8,
-    id: 'mapbox.light',
+    zoomOffset: -1,
+    id: 'mapbox/light-v10',
     accessToken: 'pk.eyJ1IjoicG5vbGwiLCJhIjoiY2pkNDNyNmtzMHRtOTMzcWZ0Y2szdzh3eCJ9.g8tszbsYH0bVCcj7v8RAEQ'
 });
 var baselayers = {
